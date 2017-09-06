9a0bff994227ea0856260f28da63a27f4936d1a2
Journey <journesky@gmail.com>
2017-09-06 18:13:39 +0800

    召唤，光能使者--玩转PathMeasure

---
+++ app/src/main/java/com/example/lvchen/myapplication/GranzortViewActivity.java
@@ -0,0 +1,13 @@
+package com.example.lvchen.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GranzortViewActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_granzort_view);
+	}
+}
